5009c3278b91b3c073b19de4a4abcc807588098b
Mahran Hassan <mahrohassan60@gmail.com>
2024-08-04 00:15:41 +0400

    dyamically added the data and the slider as well as the skeleton

--- src/components/home/NewCollections.jsx
+++ src/components/home/NewCollections.jsx
@@ -1,40 +1,101 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+import { Navigation, Pagination } from "swiper/modules";
+import Skeleton from "../ui/Skeleton";
+import CollectionCard from "../collection/CollectionCard";
 
 export default function NewCollections() {
+  const [newCollections, setNewCollections] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [swiperKey, setSwiperKey] = useState(0);
+
+  async function fetchData() {
+    setLoading(true);
+    try {
+      const { data } = await axios.get(
+        "https://remote-internship-api-production.up.railway.app/newCollections"
+      );
+      if (data === "False") {
+        setNewCollections([]);
+      } else {
+        setNewCollections(data.data);
+        setSwiperKey(swiperKey + 1);
+      }
+    } catch (error) {
+      setNewCollections([]);
+      console.error("Error fetching data:", error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
-    <section id="new-collections">
+    <section id="new-collections" data-aos="fade-up">
       <div className="container">
         <div className="row">
           <h2 className="new-collections__title">New Collections</h2>
-          <div className="new-collections__body">
-            {new Array(6).fill(0).map((_, index) => (
-              <div className="collection-column">
-                <Link to="/collection" key={index} className="collection">
-                  <img
-                    src="https://i.seadn.io/gcs/files/a5414557ae405cb6233b4e2e4fa1d9e6.jpg?auto=format&dpr=1&w=1920"
-                    alt=""
-                    className="collection__img"
-                  />
-                  <div className="collection__info">
-                    <h3 className="collection__name">Bored Ape Kennel Club</h3>
-                    <div className="collection__stats">
-                      <div className="collection__stat">
-                        <span className="collection__stat__label">Floor</span>
-                        <span className="collection__stat__data">0.46 ETH</span>
-                      </div>
-                      <div className="collection__stat">
-                        <span className="collection__stat__label">
-                          Total Volume
-                        </span>
-                        <span className="collection__stat__data">281K ETH</span>
-                      </div>
-                    </div>
-                  </div>
-                </Link>
-              </div>
-            ))}
-          </div>
+          <Swiper
+            key={swiperKey}
+            breakpoints={{
+              0: {
+                width: 0,
+                slidesPerView: 1,
+              },
+              368: {
+                width: 368,
+                slidesPerView: 1,
+              },
+              768: {
+                width: 768,
+                slidesPerView: 2,
+              },
+              1024: {
+                width: 992,
+                slidesPerView: 3,
+              },
+              1220: {
+                width: 1220,
+                slidesPerView: 4,
+              },
+              1440: {
+                width: 1440,
+                slidesPerView: 5,
+              },
+            }}
+            modules={[Navigation, Pagination]}
+            spaceBetween={12}
+            loop
+            navigation
+            slidesPerView={6}
+          >
+            <div className="new-collections__body">
+              {loading
+                ? new Array(8).fill(0).map((_, index) => (
+                    <SwiperSlide key={index}>
+                      <CollectionCard loading={true} />
+                    </SwiperSlide>
+                  ))
+                : newCollections.slice(0, 10).map((nft, index) => (
+                    <SwiperSlide key={index}>
+                      <CollectionCard
+                        loading={false}
+                        collection={nft}
+                        link={`/collection/${nft.collectionId}`}
+                      />
+                    </SwiperSlide>
+                  ))}
+            </div>
+          </Swiper>
         </div>
       </div>
     </section>
