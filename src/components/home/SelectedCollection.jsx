97cc8557bcc222d1003dbd5de79903a7eabb7375
Mahran Hassan <mahrohassan60@gmail.com>
2024-08-03 13:49:41 +0400

    dynamically added the data and added the skeleton page

--- src/components/home/SelectedCollection.jsx
+++ src/components/home/SelectedCollection.jsx
@@ -1,46 +1,83 @@
-import React from "react";
-import SelectedItemVideo from "../../assets/selected-collection.mp4";
-import SelectedItemThumbnail from "../../assets/selected-collection-thumbnail.jpg";
-import SelectedItemLogo from "../../assets/selected-collection-logo.avif";
+import React, { useEffect, useState } from "react";
 import VerifiedIcon from "../../assets/verified.png";
 import { Link } from "react-router-dom";
-
+import axios from "axios";
+import Skeleton from "../ui/Skeleton";
 export default function SelectedCollection() {
+  const [nftData, setNftData] = useState({});
+  const [loading, setLoading] = useState(false);
+
+  async function fetchData() {
+    setLoading(true)
+    try {
+      const { data } = await axios.get(
+        "https://remote-internship-api-production.up.railway.app/selectedCollection"
+      );
+      setNftData(data.data);
+      if (nftData === "False") {
+   setNftData({})
+      }
+     
+    } catch (error) {
+      setNftData(false)
+      console.error("Error fetching data:", error);
+    } finally{
+      setLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+
+
   return (
     <header>
-      <div className="selected-collection">
-        <video
-          autoPlay
-          muted
-          loop
-          playsInline
-          poster={SelectedItemThumbnail}
-          src={SelectedItemVideo}
-          className="selected-collection__bg"
-        />
-        <div className="selected-collection__description">
-          <img
-            src={SelectedItemLogo}
-            alt=""
-            className="selected-collection__logo"
-          />
-          <h1 className="selected-collection__title">
-            Coachella Throwback Merchandise Trunk
-          </h1>
-          <Link to={'/user'} className="selected-collection__author">
-            By Coachella
-            <img
-              src={VerifiedIcon}
-              className="selected-collection__author__verified"
-            />
-          </Link>
-          <div className="selected-collection__details">10 items · 3.4 ETH</div>
-          <Link to={'/collection'} className="selected-collection__button">
-            <div className="green-pulse"></div>
-            View Collection
-          </Link>
-        </div>
-      </div>
+     
+   <div className="selected-collection">
+    { loading ? (
+          <Skeleton width="100%" height="100%" border-radius="0px" />
+    ):(
+      <>
+       <video
+     autoPlay
+     muted
+     loop
+     playsInline
+     poster={nftData.thumbnail}
+     src={nftData.videoLink}
+     className="selected-collection__bg"
+   />
+   <div className="selected-collection__description">
+     <img
+       src={nftData.logo}
+       alt=""
+       className="selected-collection__logo"
+     />
+     <h1 className="selected-collection__title">{nftData.title}</h1>
+     <Link to={`/user`} className="selected-collection__author">
+       By {nftData.creator}
+       <img
+         src={VerifiedIcon}
+         className="selected-collection__author__verified"
+         alt="Verified"
+       />
+     </Link>
+     <div className="selected-collection__details">
+       {nftData.amountOfItems} · {nftData.floorPrice} ETH
+     </div>
+     <Link to={`/collection/`} className="selected-collection__button">
+       <div className="green-pulse"></div>
+       View Collection
+     </Link>
+   </div>
+      </>
+)}
+ </div>
+
+
+   
     </header>
   );
 }
