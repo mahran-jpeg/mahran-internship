654fafd1f7d26c18eca7980846a9eb69ed98633b
Mahran Hassan <mahrohassan60@gmail.com>
2024-08-11 14:53:34 +0300

    changed some skeleton pages

--- src/components/collection/CollectionItems.jsx
+++ src/components/collection/CollectionItems.jsx
@@ -1,27 +1,17 @@
-
 import { faShoppingBag } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useEffect, useState } from "react";
-import { Link, useAsyncError } from "react-router-dom";
+import { Link } from "react-router-dom";
 import Skeleton from "../ui/Skeleton";
 import PropTypes from "prop-types";
-import axios from "axios";
 import { useParams } from "react-router-dom";
+
 export default function CollectionItems({
   selectedItems,
   loading,
   setSelectedItems,
 }) {
   const { id } = useParams();
-  const [sort, setSort] = useState("");
-  const [itemDetails, setItemDetails] = useState({});
-  function sortItems() {
-    if (sort === "HIGH_TO_LOW") {
-      setSelectedItems(selectedItems.slice().sort((a, b) => b.price - a.price));
-    } else if (sort === "LOW_TO_HIGH") {
-      setSelectedItems(selectedItems.slice().sort((a, b) => a.price - b.price));
-    }
-  }
   const [visibleCount, setVisibleCount] = useState(11);
   const [sort, setSort] = useState("");
 
@@ -32,13 +22,13 @@ export default function CollectionItems({
       setSelectedItems(selectedItems.slice().sort((a, b) => a.price - b.price));
     }
   }
-  
+
   const loadMore = () => {
     setVisibleCount((prevCount) => prevCount + 6);
   };
+
   const renderSkeletons = () => {
-    return (
-      Array(12)
+    return Array(12)
       .fill(0)
       .map((_, index) => (
         <div className="item-column" key={index}>
@@ -56,25 +46,35 @@ export default function CollectionItems({
               <Skeleton width="150px" height="20px" borderRadius="4px" />
             </span>
           </div>
-
         </div>
       ));
   };
-  useEffect(()=>{
-sortItems()
-  },[sort])
+
+  useEffect(() => {
+    sortItems();
+  }, [sort]);
+
   return (
     <section id="collection-items">
       <div className="row collection-items__row">
         <div className="collection-items__header">
           <div className="collection-items__header__left">
-            <span className="collection-items__header__live">
-              <div className="green-pulse"></div>
-              Live
-            </span>
-            <span className="collection-items__header__results">
-             {selectedItems.length}{' '}results
-            </span>
+          {loading ? (
+    <Skeleton width="120px" height="16px" borderRadius="4px" />
+  ) : (
+    <span className="collection-items__header__live">
+      <div className="green-pulse"></div>
+      Live
+    </span>
+  )}
+
+  {loading ? (
+    <Skeleton width="120px" height="16px" borderRadius="4px" />
+  ) : (
+    <span className="collection-items__header__results">
+      {selectedItems.length} results
+    </span>
+  )}
           </div>
           <select
             value={sort}
@@ -83,7 +83,7 @@ sortItems()
               setSort(event.target.value);
             }}
           >
-            <option value="" default selected disabled>
+            <option value="" disabled>
               Default
             </option>
             <option value="HIGH_TO_LOW">Price high to low</option>
@@ -91,42 +91,40 @@ sortItems()
           </select>
         </div>
         <div className="collection-items__body">
-          <div className="collection-items__body">
-            {loading
-              ? renderSkeletons()
-              : selectedItems.slice(0, visibleCount).map((item) => (
-                  <div className="item-column" key={item.itemId}>
-                    <Link to={`/item/${item.itemId}`} className="item">
-                      <figure className="item__img__wrapper">
-                        <img
-                          src={item.imageLink}
-                          alt={item.title}
-                          className="item__img"
-                        />
-                      </figure>
-                      <div className="item__details">
-                        <span className="item__details__name">
-                          {item.title}
-                        </span>
-                        <span className="item__details__price">
-                          {item.price} ETH
-                        </span>
-                        <span className="item__details__last-sale">
-                          Last sale: {item.lastSale} ETH
-                        </span>
+          {loading
+            ? renderSkeletons()
+            : selectedItems.slice(0, visibleCount).map((item) => (
+                <div className="item-column" key={item.itemId}>
+                  <Link to={`/item/${item.itemId}`} className="item">
+                    <figure className="item__img__wrapper">
+                      <img
+                        src={item.imageLink}
+                        alt={item.title}
+                        className="item__img"
+                      />
+                    </figure>
+                    <div className="item__details">
+                      <span className="item__details__name">
+                        {item.title}
+                      </span>
+                      <span className="item__details__price">
+                        {item.price} ETH
+                      </span>
+                      <span className="item__details__last-sale">
+                        Last sale: {item.lastSale} ETH
+                      </span>
+                    </div>
+                    <div className="item__see-more">
+                      <button className="item__see-more__button">
+                        See More
+                      </button>
+                      <div className="item__see-more__icon">
+                        <FontAwesomeIcon icon={faShoppingBag} />
                       </div>
-                      <div className="item__see-more">
-                        <button className="item__see-more__button">
-                          See More
-                        </button>
-                        <div className="item__see-more__icon">
-                          <FontAwesomeIcon icon={faShoppingBag} />
-                        </div>
-                      </div>
-                    </Link>
-                  </div>
-                ))}
-          </div>
+                    </div>
+                  </Link>
+                </div>
+              ))}
         </div>
       </div>
       {visibleCount < selectedItems.length && (
@@ -137,13 +135,7 @@ sortItems()
     </section>
   );
 }
-CollectionItems.defaultProps = {
-  selectedCollection: [],
-};
 
-CollectionItems.propTypes = {
-  selectedCollection: PropTypes.array,
-};
 CollectionItems.propTypes = {
   selectedItems: PropTypes.array.isRequired,
   setSelectedItems: PropTypes.func.isRequired,
